0b9573edb7e32ce4642984258cf8b07085a42e2c
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-09-07 22:07:28 +0200

    moved everything to background thread

---
+++ mergeWorker.js
@@ -0,0 +1,28 @@
+import init, { merge_exposed } from "./pkg/pdf_merge2.js";
+
+let wasmReady = false;
+
+async function initWasm() {
+  await init(); // initializes the WASM module
+  wasmReady = true;
+  postMessage({ type: "ready" });
+}
+
+initWasm();
+
+onmessage = async (event) => {
+  if (!wasmReady) {
+    postMessage({ type: "error", message: "WASM not ready yet" });
+    return;
+  }
+
+  const { files, fileName } = event.data;
+
+  try {
+    const mergedPdf = merge_exposed(files);
+
+    postMessage({ type: "done", pdf: mergedPdf, fileName }, [mergedPdf.buffer]);
+  } catch (err) {
+    postMessage({ type: "error", message: err.message });
+  }
+};
